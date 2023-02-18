a3899326a28c8277c7c746e7db107db46b628136
Saransh Hardaha <hellosaranshh@gmail.com>
2023-02-18 13:06:56 +0530

    UpdateWatch

--- src/Components/VideoPlayer.jsx
+++ src/Components/VideoPlayer.jsx
@@ -2,38 +2,38 @@ import React from "react";
 import * as Icon from "react-feather";
 
 export default function VideoPlayer({ sources }) {
-  const videoControls = {
-    isMute: false,
-    isPlaying: true,
-    isFullScreen: false,
-  };
-  const sourceList = sources;
   console.log(sources);
-  const videoData = {
-    duration: 0.0,
-    currentDuration: 0.0,
-  };
   return (
     <div className="relative h-full w-full video-js">
-      <video
-        id="my-video"
-        className="video-js vjs-default-skin vjs-big-play-centered h-full w-full"
-        controls={true}
-        autoPlay={true}
-        muted={true}
-        preload="auto"
-        data-setup="{}"
-      >
-        {sources?.map((x) => (
-          <source key={x.quality} src={x?.url} type="application/x-mpegURL" />
-        ))}
-        <source
-          src="https://wwwx11.gofcdn.com/videos/hls/W6dO87EFHD9L97dvnVoaFw/1676223417/193470/f5bc111e65c74a37fe6fc136766c7b24/ep.1.1665279843.480.m3u8"
-          type="application/x-mpegURL"
-        />
-      </video>
+      {sources && (
+        <video
+          id="my-video"
+          className="video-js vjs-default-skin vjs-big-play-centered h-full w-full"
+          controls
+          preload="auto"
+          autoPlay={true}
+          muted={false}
+          data-setup='{"fluid":true}'
+        >
+          {sources.map((x) => (
+            <source key={x.quality} src={x.url} type="application/x-mpegURL" />
+          ))}
 
-      <div className="hidden absolute bottom-0 left-0 w-full flex-col">
+          <p className="vjs-no-js">
+            To view this video please enable JavaScript, and consider upgrading
+            to a web browser that
+            <a
+              href="https://videojs.com/html5-video-support/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              supports HTML5 video
+            </a>
+          </p>
+        </video>
+      )}
+
+      {/* <div className="hidden absolute bottom-0 left-0 w-full flex-col">
         <div className="flex justify-between px-4">
           <div className="flex gap-1">
             {videoControls.isPlaying ? (
@@ -57,7 +57,7 @@ export default function VideoPlayer({ sources }) {
             <Icon.MoreVertical size={18} />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/Views/Watch.jsx
+++ src/Views/Watch.jsx
@@ -50,15 +50,50 @@ function Watch() {
   }, [animeId, episodeID]);
   return (
     <>
-      <main className="flex flex-col min-h-screen pt-12 w-full mx-auto max-w-[1440px]">
-        <div className="flex w-full">
-          <div className="flex flex-col gap-6 p-6 h-full max-w-[68rem] w-full">
-            <div className="h-auto w-full">
-              {showVideo ? (
-                <VideoPlayer sources={VideoOptions?.sources} />
-              ) : (
-                <div className="w-full h-[32rem] bg-white/10 rounded animate-pulse p-1"></div>
-              )}
+      <main className="flex flex-col min-h-screen md:pt-12 w-full mx-auto max-w-[1440px]">
+        <div className="flex flex-col md:flex-row w-full">
+          <div className="flex flex-col gap-6 p-4 h-full max-w-[68rem] w-full mx-auto">
+            <div className="flex flex-col-reverse md:flex-row gap-2 md:h-[24rem] w-full md:py-4">
+              <div className="flex flex-col gap-3">
+                <div className="flex items-center px-2 justify-between text-white w-full">
+                  <h2 className="px-1 font-bold">Episodes</h2>
+                  <div className="flex gap-1 p-1 items-center rounded-full bg-white/5 hover:bg-white/10">
+                    <button className="p-1 opacity-50 group-focus-within:opacity-80">
+                      <Icon.Search size={20} />
+                    </button>
+                    <input
+                      type="text"
+                      onChange={filterEpisodes}
+                      placeholder="Search"
+                      className="p-1 pl-3 text-sm bg-transparent focus-visible:outline-none w-full transition-all"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col divide-y divide-solid divide-white/10 text-white max-h-[24rem] overflow-y-auto scrollbar-hide">
+                  {episodes?.map((ep) => (
+                    <button
+                      key={ep.id}
+                      className="flex gap-2 text-sm py-2 md:px-2 hover:bg-white/5"
+                    >
+                      <div className="flex gap-2 items-center">
+                        <div className="font-bold text-center text-sm md:text-lg w-8 md:w-14">
+                          {ep.number}
+                        </div>
+                        <div className="flex flex-col line-clamp-1 w-full">
+                          <p className="text-sm w-64 text-left">{ep.title}</p>
+                        </div>
+                      </div>
+                    </button>
+                  ))}
+                </div>
+              </div>
+              <div className="flex items-center h-full w-full">
+                {showVideo ? (
+                  <VideoPlayer sources={VideoOptions?.sources} />
+                ) : (
+                  <div className="w-full h-[20rem] bg-white/10 rounded animate-pulse py-4"></div>
+                )}
+              </div>
             </div>
             <div className="flex gap-5">
               <img
@@ -75,9 +110,7 @@ function Watch() {
                   {anime?.title?.english ? (
                     anime?.title?.english
                   ) : (
-                      <span className="w-full h-4 bg-white/10 animate-pulse text-black rounded-md">
-                        
-                    </span>
+                    <span className="w-full h-4 bg-white/10 animate-pulse text-black rounded-md"></span>
                   )}
                 </h2>
                 <p className="text-sm text-neutral-400 line-clamp-6">
@@ -92,40 +125,6 @@ function Watch() {
               Navigation={navigation}
               Animes={anime?.recommendations?.slice(0, 8)}
             />
-          </div>
-
-          <div className="flex flex-col gap-3 py-2">
-            <div className="flex flex-col items-center justify-between text-white w-full">
-              <h2 className="py-2 px-1 text-lg font-bold">Episodes</h2>
-              <div className="flex gap-1 p-1 px-2 items-center rounded-full bg-white/5 hover:bg-white/10">
-                <button className="p-1 opacity-50 group-focus-within:opacity-80">
-                  <Icon.Search size={20} />
-                </button>
-                <input
-                  type="text"
-                  onChange={filterEpisodes}
-                  placeholder="Search"
-                  className="p-1 pl-3 bg-transparent focus-visible:outline-none w-full transition-all"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col divide-y divide-solid divide-white/10 text-white max-h-[40rem] overflow-y-auto scrollbar-hide">
-              {episodes?.map((ep) => (
-                <button
-                  key={ep.id}
-                  className="flex gap-2 text-sm py-2 md:px-4 hover:bg-white/5"
-                >
-                  <div className="flex gap-2 items-center">
-                    <div className="font-bold text-center text-base md:text-xl w-12 md:w-16">
-                      {ep.number}
-                    </div>
-                    <div className="flex flex-col line-clamp-1 w-full">
-                      <p className="text-base w-64 text-left">{ep.title}</p>
-                    </div>
-                  </div>
-                </button>
-              ))}
-            </div>
           </div>
         </div>
       </main>
